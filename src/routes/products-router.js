b98283ae55fb66f617292368e091aa4522c5e941
nahuetkt <nascona@ticketek.com.ar>
2023-12-10 08:46:28 -0300

    add put and delete products

--- src/routes/products-router.js
+++ src/routes/products-router.js
@@ -8,7 +8,6 @@ const manager = new ProductManager(path);
 const router = Router();
 
 router.post('/', async (req, res)=>{
-
     try {
       const newProduct = await manager.addProduct(req.body)   
       console.log(newProduct)
@@ -39,6 +38,32 @@ router.get('/:pid', async (req, res)=>{
       res.status(404).json({message:'No existe el producto buscado'})
     }
 });
+
+
+router.delete('/:pid', async (req, res) =>{
+  const pid = req.params.pid
+    try {
+      await  manager.deleteById(pid)
+      res.json({message : 'Producto eliminado'})
+    } catch (error) {
+      res.status(404).json({message:'No existe el producto buscado'})
+    }
+
+ 
+})
+
+
+router.put('/:pid', async (req, res)=>{
+  try {
+    const updatedProduct = await manager.updateProduct(req.body)
+    res.json(updatedProduct)
+
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
 
 
 export default router;
